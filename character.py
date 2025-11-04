184f8f896f51b162d43c217144eb643edd12f83c
ridhaaah <ridhaafthm@gmail.com>
2025-11-04 11:24:35 +0530

    Add files via upload

---
+++ character.py
@@ -0,0 +1,10 @@
+text = input("Enter a string: ")
+freq = {}
+for char in text:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+print("Character frequencies:")
+for key, value in freq.items():
+    print(key, ":", value)
