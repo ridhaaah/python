c8f4431697d2d25ebe8f9ca9e4c59dbd37f0b5e2
ridhaaah <ridhaafthm@gmail.com>
2025-11-13 14:17:09 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,6 @@
+import math
+import datetime
+import random
+print("Area of circle:", math.pi * 5 * 5)
+print("Today's date:", datetime.date.today())
+print("Random number:", random.randint(1, 100))
